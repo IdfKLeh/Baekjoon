314322cd543153d818847f6ec1315637ae01585a
IdfKLeh <104423145+IdfKLeh@users.noreply.github.com>
2023-07-27 17:18:19 +0900

    [Silver III] Title: 게임, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/1072. 게임/게임.py
+++ 백준/Silver/1072. 게임/게임.py
@@ -1,3 +1,4 @@
+
 import sys
 
 finished, won = map(int, sys.stdin.readline().split())
@@ -19,3 +20,39 @@ else:
 
 
 print(real)
+"""
+여기 아래 부터는 내가 짠 코드인데,
+아마도 부동 소수점 어쩌고 때문에 오류가 나는듯 하다.
+88프로까지는 성공하거든.
+import sys
+
+finished, won = map(int, sys.stdin.readline().split())
+percentage = (100*won)//finished
+left=0
+right=1000000000
+answer = (finished-won)//2
+if percentage>=99:
+    print(-1)
+    exit()
+
+while(True):
+    answer=(left+right)/2
+    if answer%1!=0:
+        answer+=0.5
+    target = 100*(won+answer)//(finished+answer)
+    if left== answer-1:
+        if target==percentage+1:
+            break
+        else:
+            answer +=1
+            break
+    if target>=percentage+1:
+        right=answer
+    elif target<percentage+1:
+        left=answer
+    
+    
+
+
+print(int(answer))
+"""
